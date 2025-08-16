abef0828231ad8644e4184035acdfaa8d818a3e2
Menaka Devi <menakadevi3@gmail.com>
2025-08-16 21:56:35 +0530

    Add files via upload
    day1_HomeAssignments

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="John Doe";
+int age = 30;
+double salary = 55000.75;
+char gender='M';
+boolean acceptTerms = true;
+float rating = 4.5f;
+long mobileNumber = 9876543210L;
+int pinCode = 56001;
+int luckyNumber = 7;
+
+System.out.println("The Name of the Employee is "+name+"\nAge is "+age+"\nSalary is "+salary+"\nGender is "+gender+"\nAccept Terms status is "+acceptTerms+"\nThe rating is "+rating+"\nThe mobilenumber is "+mobileNumber+"\npinCode is "+pinCode+"\nluckyNumber is "+luckyNumber);
+
+
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float browserVersion = 100.2f;
+String browserName = "firefox";
+boolean isVisible= true;
+int releaseYear = 1998;
+char browserLogo = 'f';
+
+/*To print in single line System.out.println(browserVersion);
+System.out.println(browserName);
+System.out.println(isVisible);
+System.out.println(releaseYear);
+System.out.println(browserLogo);*/
+
+
+/*Printing in the same line using +(Concat) symbol
+ * 
+ */
+System.out.println("The BrowserVersion is: "+browserVersion+"\nThe browserName is:"+browserName+"\nThe isVisible is:" +isVisible+"\nThe releaseYear is:"+releaseYear+"\nThe BrowserLogo is:"+browserLogo );
+	}
+
+}
+
+
+
+
+
